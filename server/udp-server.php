6cd0c8d98ee45639df9219561d230882fa47aaf5
monsoonal <pitpoetpendulum@gmail.com>
2015-08-05 17:52:38 -0700

    Update udp-server.php

--- server/udp-server.php
+++ server/udp-server.php
@@ -64,7 +64,7 @@ class UDP_Server {
 
 }
 
-$at_data_server  = new UDP_Server( '0.0.0.0', 14550 );
+$at_data_server  = new UDP_Server( '0.0.0.0', 49509 );
 //$nav_data_server = new UDP_Server( '0.0.0.0', 5554 );
 $at_data_server->set_debug( true );
 
