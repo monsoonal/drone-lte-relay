1276b939cc84b5e316995fd5c40625713c9b15eb
monsoonal <pitpoetpendulum@gmail.com>
2015-08-06 10:47:00 -0700

    Update udp-relayer.php

--- iPhone/udp-relayer.php
+++ iPhone/udp-relayer.php
@@ -80,7 +80,7 @@ class UDP_Relay {
 }
 
 $drone_ip       = '192.168.0.99';
-$server_ip      = gethostbyaddr( '192.168.0.100' );
+$server_ip      = gethostbyaddr( '192.168.0.102' );
 $at_data_relay  = new UDP_Relay( $drone_ip, 14550, $server_ip, 14550 );
 //$nav_data_relay = new UDP_Relay( $drone_ip, 5554, $server_ip, 5554 );
 
